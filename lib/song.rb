ca378b4822afabcbd737e79ba5fff71cf02f7f48
Regina Furness <skritschy@gmail.com>
2020-04-20 16:46:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,4 +37,15 @@ class Song
     genre_count_hash
   end 
   
+  def self.artist_count
+    artist_count_hash = {}
+    @@artists.each do |artist|
+      if artist_count_hash.include?(artist)
+        artist_count_hash[artist] += 1 
+      else
+        artist_count_hash[artist] = 1
+      end
+      artist_count_hash
+  end 
+  
 end 
