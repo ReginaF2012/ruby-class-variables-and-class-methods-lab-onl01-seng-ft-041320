cd67ce1135ffae2b29f97b7e157dd47f021c3360
Regina Furness <skritschy@gmail.com>
2020-04-20 16:40:50 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+class Song 
+  attr_reader :name, :artist, :genre
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1 
+    @@genres << genre
+    @@artists << artist
+  end
+  
+  def self.count
+    @@count
+  end 
+  
+  def self.artists
+    @@artists.uniq!
+  end 
+   
+   def self.genres
+     @@genres.uniq!
+   end 
+   
+   
+  def self.genre_count
+    genre_count_hash = {}
+    @@genres.each do |genre|
+      if genre_count.include?(genre)
+        genre[count] += 1
+      else
+        genre[count] = 1
+      end
+    end 
+  end 
+  
+end 
